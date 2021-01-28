f40e047ae5eafb50ab9f7bff2a151ac29cb687a6
Noah Husby <noahhusby@gmail.com>
2021-01-28 16:51:23 -0500

    Added RetrieveUncachedPlayerPacket to controller

--- controller/src/main/java/com/noahhusby/terrabungee/controller/network/S2C/S2CRetrieveUncachedPlayerPacket.java
+++ controller/src/main/java/com/noahhusby/terrabungee/controller/network/S2C/S2CRetrieveUncachedPlayerPacket.java
@@ -2,12 +2,15 @@ package com.noahhusby.terrabungee.controller.network.S2C;
 
 import com.google.gson.JsonObject;
 import com.noahhusby.terrabungee.api.Constants;
+import com.noahhusby.terrabungee.api.TerraBungeeUtil;
+import com.noahhusby.terrabungee.api.players.TBPlayer;
 import com.noahhusby.terrabungee.controller.console.ConsoleColor;
 import com.noahhusby.terrabungee.controller.console.TerraBungeeConsole;
 import com.noahhusby.terrabungee.controller.console.TextComponent;
 import com.noahhusby.terrabungee.controller.network.IS2CPacket;
 import com.noahhusby.terrabungee.controller.network.Response;
 import com.noahhusby.terrabungee.controller.network.ServicePacket;
+import com.noahhusby.terrabungee.controller.players.PlayerManager;
 
 import java.util.UUID;
 
@@ -22,9 +25,19 @@ public class S2CRetrieveUncachedPlayerPacket implements IS2CPacket {
         String name = data.get("name").getAsString();
         try {
             UUID uuid =  UUID.fromString(name);
-            //TODO: Check for player
+            TBPlayer player = PlayerManager.getInstance().getPlayersRegistry().get(uuid);
+            if(player == null) throw new IllegalArgumentException();
+            response.setData(TerraBungeeUtil.GSON.toJsonTree(player).getAsJsonObject());
+            return;
         } catch (IllegalArgumentException e) {
-
+            for(TBPlayer player : PlayerManager.getInstance().getPlayersRegistry().values()) {
+                if(player.getName().equalsIgnoreCase(name)) {
+                    response.setData(TerraBungeeUtil.GSON.toJsonTree(player).getAsJsonObject());
+                    return;
+                }
+            }
         }
+
+        response.setCode(com.noahhusby.terrabungee.api.network.Response.ResponseCode.ERROR);
     }
 }
