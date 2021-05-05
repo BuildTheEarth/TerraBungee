cb5396307daad708157a4dae8b38c38118332de1
Noah Husby <noahhusby@gmail.com>
2021-05-05 15:27:04 -0400

    Cleaned up

--- client/src/main/java/net/buildtheearth/terrabungee/client/TerraBungeeAPI.java
+++ client/src/main/java/net/buildtheearth/terrabungee/client/TerraBungeeAPI.java
@@ -31,49 +31,4 @@ public class TerraBungeeAPI {
     public static TerraBungeeClient createService(@NonNull ServiceType serviceType, @NonNull String Id, @NonNull String controller) {
         return new TerraBungeeClient(serviceType, Id, controller);
     }
-
-    public static void main(String[] args) {
-        TerraBungeeClient client = createService(ServiceType.CUSTOM, "TEST", "144.217.77.29:7500");
-        client.connect();
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        CompletableFuture<Response> response = client.getNetworkManager().send(new IS2CPacket() {
-            @Override
-            public String getType() {
-                return "discord_link_request";
-            }
-
-            @Override
-            public void getMessage(TerraBungeeClient instance, JsonObject data) {
-                data.addProperty("uuid", "4cfa7dc1-3021-42b0-969b-224a9656cc6d");
-            }
-        });
-
-        response.thenAccept(response1 -> {
-           System.out.println(response1.getCode().name());
-           System.out.println(new Gson().toJson(response1.getData()));
-        });
-
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        client.getNetworkManager().send(new IS2CPacket() {
-            @Override
-            public String getType() {
-                return "discord_message";
-            }
-
-            @Override
-            public void getMessage(TerraBungeeClient instance, JsonObject data) {
-                data.addProperty("uuid", "4cfa7dc1-3021-42b0-969b-224a9656cc6d");
-                data.addProperty("message", "Some amazing magical message! **Hello There** :joy:");
-            }
-        });
-    }
 }
