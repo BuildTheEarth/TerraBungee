982b91244afff61cd0bfa2956b2251aa20113120
Noah Husby <noahhusby@gmail.com>
2021-09-07 14:29:25 -0500

    Updated space jdk to 11

--- .space.kts
+++ .space.kts
@@ -1,6 +1,6 @@
 
 job("Publish") {
-    container(displayName = "Run publish script", image = "gradle") {
+    container(displayName = "Run publish script", image = "gradle:jdk11") {
         kotlinScript { api ->
             api.gradle("publish")
         }
