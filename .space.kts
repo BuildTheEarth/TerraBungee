a218b1939cc39511c17b5efc32c544309c4ba01f
Noah Husby <32528627+noahhusby@users.noreply.github.com>
2021-11-02 10:33:37 -0500

    Delete .space.kts

--- .space.kts
+++
@@ -1,8 +0,0 @@
-
-job("Publish") {
-    container(displayName = "Run publish script", image = "gradle:jdk11") {
-        kotlinScript { api ->
-            api.gradle("publish")
-        }
-    }
-}
