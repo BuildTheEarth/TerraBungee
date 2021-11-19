7be364574a7391be8676c5cc0bd94aa6ca9aaaf2
Noah Husby <noahhusby@gmail.com>
2021-11-18 20:53:38 -0600

    Replaced Forge config handler with HusbyLib

--- controller/src/main/java/net/buildtheearth/terrabungee/controller/config/ConfigHandler.java
+++ controller/src/main/java/net/buildtheearth/terrabungee/controller/config/ConfigHandler.java
@@ -1,7 +1,7 @@
 package net.buildtheearth.terrabungee.controller.config;
 
 import com.google.common.collect.Maps;
-import com.noahhusby.lib.data.sql.Credentials;
+import com.noahhusby.lib.application.config.Configuration;
 import com.noahhusby.lib.data.sql.MySQL;
 import com.noahhusby.lib.data.sql.structure.Structure;
 import com.noahhusby.lib.data.sql.structure.Type;
@@ -12,15 +12,13 @@ import com.noahhusby.lib.data.storage.handlers.LocalStorageHandler;
 import com.noahhusby.lib.data.storage.handlers.SQLStorageHandler;
 import lombok.Getter;
 import lombok.SneakyThrows;
+import net.buildtheearth.api.TerraBungee;
 import net.buildtheearth.terrabungee.controller.TerraBungeeController;
 import net.buildtheearth.terrabungee.controller.discord.DiscordManager;
 import net.buildtheearth.terrabungee.controller.players.PlayerManager;
 import net.buildtheearth.terrabungee.controller.services.InstanceManager;
-import net.minecraftforge.common.config.Configuration;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
@@ -44,31 +42,11 @@ public class ConfigHandler {
     private File discordGuildFile;
     private File discordBotFile;
 
-    private Configuration config;
-
-    private String category;
-    Map<String, List<String>> categories = Maps.newHashMap();
-
-    public static String host;
-    public static int port;
-
-    public static String sqlHost;
-    public static int sqlPort;
-    public static String sqlUser;
-    public static String sqlPassword;
-    public static String sqlDb;
-    public static boolean localEnabled;
-
     @Getter
     private Map<String, Storage> storageMap = Maps.newHashMap();
 
     private void init() {
-        File configFile = new File(System.getProperty("user.dir"), "terrabungee.cfg");
-        if (!configFile.exists()) {
-            TerraBungeeController.logger.warning("Generating a new config file! Please fill out terrabungee.cfg before continuing.");
-        }
-
-        File localDb = new File(System.getProperty("user.dir"), "local");
+        File localDb = new File(TerraBungee.getInstance().getFolder(), "local");
         if (!localDb.exists()) {
             localDb.mkdir();
         }
@@ -79,7 +57,6 @@ public class ConfigHandler {
         punishmentFile = new File(localDb, "punishments.json");
         discordBotFile = new File(localDb, "discord_bots.json");
 
-        config = new Configuration(configFile);
         loadData();
     }
 
@@ -88,27 +65,27 @@ public class ConfigHandler {
      */
     @SneakyThrows
     public void loadData() {
-        config.load();
 
-        cat("General", "General settings for the TerraBungee Controller");
-        host = config.getString(prop("Host"), category, "127.0.0.1", "The IP address that the controller should run on.");
-        port = config.getInt(prop("Port"), category, 7000, 0, 65535, "The port that the controller should run on.");
+    }
 
-        order();
+    @SneakyThrows
+    public void load() {
+        Configuration configuration = Configuration.of(TerraBungeeConfig.class);
+        configuration.sync(TerraBungeeConfig.class);
+        loadHandlers();
+    }
 
-        cat("MySQL Database", "Settings for the MySQL Database");
-        sqlHost = config.getString(prop("Host"), category, "127.0.0.1", "The host IP for the database.");
-        sqlPort = config.getInt(prop("Port"), category, 3306, 0, 65535, "The port for the database.");
-        sqlUser = config.getString(prop("Username"), category, "", "The username for the database.");
-        sqlPassword = config.getString(prop("Password"), category, "", "The password for the database.");
-        sqlDb = config.getString(prop("Database"), category, "", "The name of the database.");
-        localEnabled = config.getBoolean(prop("Local Storage"), category, false, "Should data be stored locally as well");
+    @SneakyThrows
+    public void unload() {
+        PlayerManager.getInstance().getPlayers().close();
+        PlayerManager.getInstance().getPunishments().close();
+        InstanceManager.getInstance().getStaticInstances().close();
+        DiscordManager.getInstance().getGuildConfigs().close();
+        DiscordManager.getInstance().getBotConfigs().close();
+    }
 
-        order();
-        if (config.hasChanged()) {
-            config.save();
-        }
-
+    @SneakyThrows
+    public void loadHandlers() {
         Storage playerData = PlayerManager.getInstance().getPlayers();
         playerData.close();
         ((StorageHashMap) playerData).clear();
@@ -129,7 +106,9 @@ public class ConfigHandler {
         discordBotConfigData.clearHandlers();
         ((StorageHashMap) discordBotConfigData).clear();
 
-        if (localEnabled) {
+        TerraBungeeConfig.DatabaseOptions databaseOptions = TerraBungeeConfig.database;
+
+        if (databaseOptions.localStorage) {
             playerData.registerHandler(new LocalStorageHandler(playerDataFile));
             playerData.registerHandler(new LocalStorageHandler(punishmentFile));
             staticInstanceData.registerHandler(new LocalStorageHandler(staticInstanceFile));
@@ -138,8 +117,7 @@ public class ConfigHandler {
         }
 
         {
-            SQLStorageHandler sqlStorageHandler = new SQLStorageHandler(new MySQL(
-                    new Credentials(sqlHost, sqlPort, sqlUser, sqlPassword, sqlDb)), "Players",
+            SQLStorageHandler sqlStorageHandler = new SQLStorageHandler(new MySQL(databaseOptions.toCredentials()), "Players",
                     Structure.builder()
                             .add("UUID", Type.TEXT)
                             .add("Name", Type.TEXT)
@@ -154,8 +132,7 @@ public class ConfigHandler {
         }
 
         {
-            SQLStorageHandler sqlStorageHandler = new SQLStorageHandler(new MySQL(
-                    new Credentials(sqlHost, sqlPort, sqlUser, sqlPassword, sqlDb)), "Punishments",
+            SQLStorageHandler sqlStorageHandler = new SQLStorageHandler(new MySQL(databaseOptions.toCredentials()), "Punishments",
                     Structure.builder()
                             .add("Id", Type.INT)
                             .add("Type", Type.TEXT)
@@ -173,8 +150,7 @@ public class ConfigHandler {
         }
 
         {
-            SQLStorageHandler sqlStorageHandler = new SQLStorageHandler(new MySQL(
-                    new Credentials(sqlHost, sqlPort, sqlUser, sqlPassword, sqlDb)), "StaticInstances",
+            SQLStorageHandler sqlStorageHandler = new SQLStorageHandler(new MySQL(databaseOptions.toCredentials()), "StaticInstances",
                     Structure.builder()
                             .add("Id", Type.TEXT)
                             .add("Address", Type.TEXT)
@@ -186,8 +162,7 @@ public class ConfigHandler {
         }
 
         {
-            SQLStorageHandler sqlStorageHandler = new SQLStorageHandler(new MySQL(
-                    new Credentials(sqlHost, sqlPort, sqlUser, sqlPassword, sqlDb)), "DiscordGuilds",
+            SQLStorageHandler sqlStorageHandler = new SQLStorageHandler(new MySQL(databaseOptions.toCredentials()), "DiscordGuilds",
                     Structure.builder()
                             .add("GuildID", Type.TEXT)
                             .add("BotID", Type.INT)
@@ -201,8 +176,7 @@ public class ConfigHandler {
         }
 
         {
-            SQLStorageHandler sqlStorageHandler = new SQLStorageHandler(new MySQL(
-                    new Credentials(sqlHost, sqlPort, sqlUser, sqlPassword, sqlDb)), "DiscordBots",
+            SQLStorageHandler sqlStorageHandler = new SQLStorageHandler(new MySQL(databaseOptions.toCredentials()), "DiscordBots",
                     Structure.builder()
                             .add("Id", Type.INT)
                             .add("Name", Type.TEXT)
@@ -239,7 +213,7 @@ public class ConfigHandler {
      */
     public void reload() {
         TerraBungeeController.logger.info("Reloading the controller!");
-        loadData();
+        load();
     }
 
     /**
@@ -249,22 +223,5 @@ public class ConfigHandler {
         PlayerManager.getInstance().getPlayers().migrate(0);
         InstanceManager.getInstance().getStaticInstances().migrate(0);
         DiscordManager.getInstance().getGuildConfigs().migrate(0);
-    }
-
-    private String prop(String n) {
-        categories.get(category).add(n);
-        return n;
-    }
-
-    private void cat(String category, String comment) {
-        this.category = category;
-        if (!categories.containsKey(category)) {
-            categories.put(category, new ArrayList<>());
-        }
-        config.addCustomCategoryComment(category, comment);
-    }
-
-    private void order() {
-        config.setCategoryPropertyOrder(category, categories.get(category));
     }
 }
---
+++ controller/src/main/java/net/buildtheearth/terrabungee/controller/config/TerraBungeeConfig.java
@@ -0,0 +1,75 @@
+package net.buildtheearth.terrabungee.controller.config;
+
+import com.noahhusby.lib.application.config.Config;
+import com.noahhusby.lib.application.config.Config.Comment;
+import com.noahhusby.lib.application.config.Config.Name;
+import com.noahhusby.lib.application.config.Config.Type;
+import com.noahhusby.lib.data.sql.Credentials;
+
+/**
+ * @author Noah Husby
+ */
+@Config(name = "terrabungee", type = Type.HOCON)
+public class TerraBungeeConfig {
+
+    @Comment({
+            "General settings for the TerraBungee Controller"
+    })
+    public static GeneralOptions general = new GeneralOptions();
+
+    public static class GeneralOptions {
+        @Comment({
+                "The IP address that the controller should run on. [default: 127.0.0.1]"
+        })
+        @Name("Host")
+        public String host = "127.0.0.1";
+
+        @Comment({
+                "The port that the controller should run on. [range: 0 ~ 65535, default: 7000]"
+        })
+        @Name("Port")
+        public int port = 7000;
+    }
+
+    @Comment({
+            "Settings for the MySQL Database"
+    })
+    public static DatabaseOptions database = new DatabaseOptions();
+
+    public static class DatabaseOptions {
+        @Comment({
+                "The host IP for the database."
+        })
+        public String sqlHost = "127.0.0.1";
+
+        @Comment({
+                "The port for the database."
+        })
+        public int sqlPort = 3306;
+
+        @Comment({
+                "The username for the database."
+        })
+        public String sqlUser = "";
+
+        @Comment({
+                "The password for the database."
+        })
+        public String sqlPassword = "";
+
+        @Comment({
+                "The name of the database."
+        })
+        public String sqlDb = "";
+
+        @Comment({
+                "Should data be stored locally as well [default: false]"
+        })
+        @Name("Local Storage")
+        public boolean localStorage = false;
+
+        public Credentials toCredentials() {
+            return new Credentials(sqlHost, sqlPort, sqlUser, sqlPassword, sqlDb);
+        }
+    }
+}
