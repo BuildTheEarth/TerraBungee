ccac3466e18713ade413f27572d239f099102cd4
XboxBedrock <xboxbedrock@gmail.com>
2024-07-17 22:23:11 -0700

    Fix utils

--- velocity/src/main/java/net/buildtheearth/terrabungee/proxy/util/ProxyUtil.java
+++ velocity/src/main/java/net/buildtheearth/terrabungee/proxy/util/ProxyUtil.java
@@ -5,16 +5,20 @@
 
 package net.buildtheearth.terrabungee.proxy.util;
 
-import com.noahhusby.terrabungee.proxy.TerraBungeeProxy;
+import com.google.common.collect.Maps;
+import com.velocitypowered.api.proxy.server.RegisteredServer;
+import net.buildtheearth.terrabungee.proxy.TerraBungeeProxy;
 import lombok.experimental.UtilityClass;
 import net.buildtheearth.terrabungee.common.TerraBungeeUtil;
-import net.md_5.bungee.api.ProxyServer;
-import net.md_5.bungee.api.config.ServerInfo;
+import com.velocitypowered.api.proxy.server.ServerInfo;
 
 import java.net.InetSocketAddress;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 /**
  * @author Noah Husby
@@ -29,28 +33,30 @@ public class ProxyUtil {
     }
 
     public static void addServer(String instanceId, String address) {
-        if (ProxyServer.getInstance().getServerInfo(instanceId) != null) {
-            TerraBungeeProxy.LOGGER.info("Instance " + instanceId + " tried to be created but already existed?! Ignoring request.");
+        if (TerraBungeeProxy.getServer().getServer(instanceId).isPresent()) {
+            TerraBungeeProxy.LOGGER.info("Instance {} tried to be created but already existed?! Ignoring request.", instanceId);
             return;
         }
 
         InetSocketAddress inetSocketAddress = TerraBungeeUtil.makeInetSocketAddress(address);
-        ServerInfo serverInfo = ProxyServer.getInstance().constructServerInfo(
-                instanceId, inetSocketAddress,
-                "A TerraBungee Instance - " + instanceId, false
-        );
 
-        ProxyServer.getInstance().getServers().put(instanceId, serverInfo);
-        TerraBungeeProxy.LOGGER.info("Added instance " + instanceId + " on address " + address);
+        ServerInfo toRegister = new ServerInfo(instanceId, inetSocketAddress);
+
+        TerraBungeeProxy.getServer().registerServer(toRegister);
+
+        TerraBungeeProxy.LOGGER.info("Added instance {} on address {}", instanceId, address);
     }
 
     public static void removeServer(String instanceId) {
-        if (ProxyServer.getInstance().getServerInfo(instanceId) == null) {
-            TerraBungeeProxy.LOGGER.info("Instance " + instanceId + " tried to be deleted but didn't exist?! Ignoring request.");
+        Optional<RegisteredServer> perhapsRegisteredServer = TerraBungeeProxy.getServer().getServer(instanceId);
+
+        if (!perhapsRegisteredServer.isPresent()) {
+            TerraBungeeProxy.LOGGER.info("Instance {} tried to be deleted but didn't exist?! Ignoring request.", instanceId);
             return;
         }
 
-        ProxyServer.getInstance().getServers().remove(instanceId);
+        TerraBungeeProxy.getServer().unregisterServer(perhapsRegisteredServer.get().getServerInfo());
+
         TerraBungeeProxy.LOGGER.info("Removed instance " + instanceId);
     }
 
@@ -69,5 +75,15 @@ public class ProxyUtil {
             return false;
         }
         return string.regionMatches(true, 0, prefix, 0, prefix.length());
+    }
+
+    public static Map<String, RegisteredServer> getRegisteredServers() {
+        Map<String, RegisteredServer> servers = new HashMap<>();
+
+        TerraBungeeProxy.getServer().getAllServers().forEach((value) -> {
+            servers.put(value.getServerInfo().getName(), value);
+        });
+
+        return servers;
     }
 }
