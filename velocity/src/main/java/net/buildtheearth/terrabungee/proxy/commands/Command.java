cb049c5ea19e906bf8d52de82a3937d4adb0168c
MineFact <minefact.minecraft@gmail.com>
2025-02-15 21:10:08 +0100

    re-added ChatUtil and Command as it does make sense

---
+++ velocity/src/main/java/net/buildtheearth/terrabungee/proxy/commands/Command.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2020 Noah Husby
+ * TerraBungeeProxy - Command.java
+ */
+
+package net.buildtheearth.terrabungee.proxy.commands;
+
+import com.velocitypowered.api.command.CommandSource;
+import com.velocitypowered.api.command.SimpleCommand;
+
+public abstract class Command implements SimpleCommand {
+    private String permissionNode;
+    private String name;
+
+    public Command(String name, String node) {
+        super();
+        this.name = name;
+        this.permissionNode = node;
+    }
+
+    public Command(String name, String node, String[] alias) {
+        super();
+        this.name = name;
+        this.permissionNode = node;
+    }
+
+    protected boolean hasAdmin(CommandSource sender) {
+        return sender.hasPermission("terrabungee.admin");
+    }
+
+    protected boolean hasPermissionAdmin(CommandSource sender) {
+        return sender.hasPermission("terrabungee.admin") || sender.hasPermission(permissionNode + ".admin");
+    }
+}
---
+++ velocity/src/main/java/net/buildtheearth/terrabungee/proxy/util/ChatUtil.java
@@ -0,0 +1,163 @@
+/*
+ * Copyright (c) 2025 BuildTheEarth
+ * TerraBungeeProxy - ChatUtil.java
+ */
+
+package net.buildtheearth.terrabungee.proxy.util;
+
+import com.velocitypowered.api.command.CommandSource;
+import net.buildtheearth.terrabungee.proxy.Constants;
+import lombok.experimental.UtilityClass;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
+
+import java.util.Arrays;
+
+/**
+ * @author Noah Husby, MineFact
+ */
+@UtilityClass
+public class ChatUtil {
+
+    // ======== PREDEFINED MESSAGES ========
+
+    public static TextComponent PREFIX = LegacyComponentSerializer.legacyAmpersand().deserialize(Constants.prefix);
+
+    public static TextComponent PLAYER_ONLY = titleAndCombineError("This command can only be executed by players!");
+    public static TextComponent NO_CONTROLLER_CONTACT = titleAndCombineError("Unable to contact the controller! Please try again");
+    public static TextComponent NO_PERMISSION = titleAndCombineError("You do not have permission to use this command");
+
+
+
+    // ======== PREDEFINED MESSAGES WITH PARAMETERS ========
+
+    public static TextComponent USAGE(String command) {
+        return titleAndCombineError("Usage: /" + command);
+    }
+
+    public static TextComponent NOT_A_NUMBER(String number) {
+        return titleAndCombineError(number, " is not a valid number");
+    }
+
+    public static TextComponent PLAYER_NEVER_JOINED(String playerName) {
+        return titleAndCombine(NamedTextColor.YELLOW, playerName, NamedTextColor.GRAY, " has never joined the network!");
+    }
+
+    public static TextComponent PLAYER_HAS_NO_PUNISHMENTS(String playerName) {
+        return titleAndCombine(NamedTextColor.YELLOW, playerName, NamedTextColor.GRAY, " has no punishments on record!");
+    }
+
+
+
+    // ======== HELPER METHODS ========
+
+    /**
+     * A message that is combined by the given objects with the global prefix.
+     *
+     * @param objects the objects to combine. Possible types: {@link Component}, {@link String}, {@link TextColor}, {@link TextDecoration}
+     * @return prefix followed by the given objects
+     */
+    public static TextComponent titleAndCombine(Object... objects) {
+        return combine(true, objects);
+    }
+
+    /**
+     * A red error message that is combined by the given objects with the global prefix.
+     *
+     * @param objects the objects to combine. Possible types: {@link Component}, {@link TextColor}, {@link TextDecoration}
+     * @return prefix followed by the given objects in red if not colored otherwise
+     */
+    public static TextComponent titleAndCombineError(Object... objects) {
+        return combine(true, NamedTextColor.RED, objects);
+    }
+
+    /**
+     * A message that is combined by the given objects.
+     *
+     * @param objects the objects to combine. Possible types: {@link Component}, {@link String}, {@link TextColor}, {@link TextDecoration}
+     * @return the given objects combined
+     */
+    public static TextComponent combine(Object... objects) {
+        return combine(false, objects);
+    }
+
+    /**
+     * A message that is combined by the given objects.
+     *
+     * @param title if true, the message will be combined with the global prefix
+     * @param objects the objects to combine. Possible types: {@link Component}, {@link String}, {@link TextColor}, {@link TextDecoration}
+     * @return the given objects combined
+     */
+    public static TextComponent combine(boolean title, Object... objects) {
+        TextComponent textComponent = title ? PREFIX : Component.text().build();
+        StringBuilder builder = null;
+        TextColor lastFormat = null;
+        TextDecoration lastDecoration = null;
+        for (Object o : objects) {
+            if (o instanceof Component) {
+                if (builder != null) {
+                    textComponent = textComponent.append(Component.text(builder.toString(), lastFormat));
+
+                    if(lastDecoration != null)
+                        textComponent = textComponent.decorate(lastDecoration);
+
+                    builder = null;
+                }
+            } else {
+                if (o instanceof NamedTextColor)
+                    lastFormat = (NamedTextColor) o;
+                else if (o instanceof TextDecoration)
+                    lastDecoration = (TextDecoration) o;
+
+                if (builder == null)
+                    builder = new StringBuilder();
+
+                builder.append(o);
+            }
+        }
+
+        if (builder != null) {
+            textComponent = textComponent.append(Component.text(builder.toString()));
+        }
+        return textComponent;
+    }
+
+    /**
+     * Sends a message box with the given title and text to the player
+     *
+     * @param sender The player to send the message to
+     * @param title The title of the message
+     * @param text The text of the message
+     */
+    public static void sendMessageBox(CommandSource sender, String title, Component text) {
+        sendMessageBox(sender, title, () -> sender.sendMessage(text));
+    }
+
+    /**
+     * Sends a message box with the given title and text to the player.
+     * With the given runnable multiple messages can be sent inside the message box.
+     *
+     * @param sender The player to send the message to
+     * @param title The title of the message
+     * @param runnable The runnable that sends the messages inside the message box
+     */
+    public static void sendMessageBox(CommandSource sender, String title, Runnable runnable) {
+        sender.sendMessage(combine(NamedTextColor.DARK_GRAY + "" + TextDecoration.STRIKETHROUGH, "==============", NamedTextColor.DARK_GRAY, " " + TextDecoration.BOLD + title + " ", NamedTextColor.DARK_GRAY + "" + TextDecoration.STRIKETHROUGH, "=============="));
+        sender.sendMessage(Component.text().build());
+
+        runnable.run();
+
+        // Strips the color from the title
+        int length = PlainTextComponentSerializer.plainText().serialize(LegacyComponentSerializer.legacySection().deserialize(title)).length();
+        char[] array = new char[length];
+        Arrays.fill(array, '=');
+        String bottom = "==============================" + new String(array);
+        sender.sendMessage(Component.text().build());
+        sender.sendMessage(combine(NamedTextColor.DARK_GRAY + "" + TextDecoration.STRIKETHROUGH, bottom));
+    }
+}
