ab3c4fa965e8db0bc86e3b328b52d898971b1972
MineFact <minefact.minecraft@gmail.com>
2025-02-15 21:13:32 +0100

    fixed RemoveInstanceFragment

--- velocity/src/main/java/net/buildtheearth/terrabungee/proxy/commands/fragments/instance/RemoveInstanceFragment.java
+++ velocity/src/main/java/net/buildtheearth/terrabungee/proxy/commands/fragments/instance/RemoveInstanceFragment.java
@@ -3,40 +3,44 @@
  * TerraBungeeProxy - RemoveInstanceFragment.java
  */
 
-package com.noahhusby.terrabungee.proxy.commands.fragments.instance;
+package net.buildtheearth.terrabungee.proxy.commands.fragments.instance;
 
-import com.noahhusby.terrabungee.proxy.TerraBungeeProxy;
-import com.noahhusby.terrabungee.proxy.commands.ICommandFragment;
-import com.noahhusby.terrabungee.proxy.util.ChatUtil;
+import com.velocitypowered.api.command.CommandSource;
+import com.velocitypowered.api.command.SimpleCommand;
+import net.buildtheearth.terrabungee.proxy.TerraBungeeProxy;
+import net.buildtheearth.terrabungee.proxy.commands.ICommandFragment;
+import net.buildtheearth.terrabungee.proxy.util.ChatUtil;
 import net.buildtheearth.terrabungee.client.network.S2C.S2CRemoveStaticInstancePacket;
 import net.buildtheearth.terrabungee.common.network.Response;
-import net.md_5.bungee.api.ChatColor;
-import net.md_5.bungee.api.CommandSender;
+import net.kyori.adventure.text.format.NamedTextColor;
 
 import java.util.concurrent.CompletableFuture;
 
 public class RemoveInstanceFragment implements ICommandFragment {
     @Override
-    public void execute(CommandSender sender, String[] args) {
+    public void execute(SimpleCommand.Invocation invocation) {
+        CommandSource sender = invocation.source();
+        String[] args = invocation.arguments();
+
         if (args.length < 1) {
-            sender.sendMessage(ChatUtil.titleAndCombine(ChatColor.RED, "Usage: /tba instance remove <id>"));
+            sender.sendMessage(ChatUtil.USAGE("/tba instance remove <id>"));
             return;
         }
 
         CompletableFuture<Response> response = TerraBungeeProxy.getInstance().getTerraBungee().getNetworkManager().send(new S2CRemoveStaticInstancePacket(args[0]));
         response.thenAccept(r -> {
             if (r.getCode() == Response.ResponseCode.TIMED_OUT) {
-                sender.sendMessage(ChatUtil.titleAndCombine(ChatColor.RED, "The controller was unable to be contacted. Please check the connection and try again."));
+                sender.sendMessage(ChatUtil.titleAndCombine(NamedTextColor.RED, "The controller was unable to be contacted. Please check the connection and try again."));
                 return;
             }
 
             if (r.getCode() == Response.ResponseCode.ERROR) {
-                sender.sendMessage(ChatUtil.titleAndCombine(ChatColor.GRAY, "Could not find static instance ", ChatColor.RED, args[0].toLowerCase(), ChatColor.GRAY, "!"));
+                sender.sendMessage(ChatUtil.titleAndCombine(NamedTextColor.GRAY, "Could not find static instance ", NamedTextColor.RED, args[0].toLowerCase(), NamedTextColor.GRAY, "!"));
                 return;
             }
 
-            sender.sendMessage(ChatUtil.titleAndCombine(ChatColor.GREEN, "Successfully removed static instance ",
-                    ChatColor.RED, args[0].toLowerCase()));
+            sender.sendMessage(ChatUtil.titleAndCombine(NamedTextColor.GREEN, "Successfully removed static instance ",
+                    NamedTextColor.RED, args[0].toLowerCase()));
         });
 
     }
