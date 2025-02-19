b4c3b11d9957ccfc033d983f1d9c4aef58341288
MineFact <minefact.minecraft@gmail.com>
2025-02-19 14:06:33 +0100

    add documentation for ProxyUtil

--- proxy-velocity/src/main/java/net/buildtheearth/terrabungee/proxy/util/ProxyUtil.java
+++ proxy-velocity/src/main/java/net/buildtheearth/terrabungee/proxy/util/ProxyUtil.java
@@ -31,6 +31,13 @@ public class ProxyUtil {
         return date.format(timeFormat);
     }
 
+    /**
+     * Registers a server with the given instance id and address to the proxy. If
+     * the server already exists, the request is ignored.
+     *
+     * @param instanceId the id of the instance to add
+     * @param address    the address of the instance to add
+     */
     public static void addServer(String instanceId, String address) {
         if (TerraBungeeProxy.getServer().getServer(instanceId).isPresent()) {
             TerraBungeeProxy.LOGGER.info("Instance {} tried to be created but already existed?! Ignoring request.", instanceId);
@@ -46,6 +53,12 @@ public class ProxyUtil {
         TerraBungeeProxy.LOGGER.info("Added instance {} on address {}", instanceId, address);
     }
 
+    /**
+     * Unregisters a server with the given instance id from the proxy. If
+     * the server does not exist, the request is ignored.
+     *
+     * @param instanceId the id of the instance to remove
+     */
     public static void removeServer(String instanceId) {
         Optional<RegisteredServer> perhapsRegisteredServer = TerraBungeeProxy.getServer().getServer(instanceId);
 
@@ -59,6 +72,17 @@ public class ProxyUtil {
         TerraBungeeProxy.LOGGER.info("Removed instance " + instanceId);
     }
 
+    /**
+     * Copies all strings in the given iterable that start with the given
+     * {@code token} (case insensitive) to the given collection.
+     *
+     * @param token      the string prefix to search for
+     * @param originals  the strings to search through
+     * @param collection the collection to add strings to
+     * @return the collection with the added strings
+     * @throws UnsupportedOperationException if the given collection does not support the add operation
+     * @throws IllegalArgumentException       if the given collection is null
+     */
     public static <T extends Collection<? super String>> T copyPartialMatches(final String token, final Iterable<String> originals, final T collection) throws UnsupportedOperationException, IllegalArgumentException {
         for (String string : originals) {
             if (startsWithIgnoreCase(string, token)) {
@@ -69,6 +93,16 @@ public class ProxyUtil {
         return collection;
     }
 
+    /**
+     * Returns true if the given string starts with the given prefix,
+     * ignoring the case of both strings.
+     *
+     * @param string the string to check
+     * @param prefix the prefix to check against
+     * @return true if the string starts with the prefix (ignoring case)
+     * @throws NullPointerException if either parameter is null
+     * @throws IllegalArgumentException if the string is shorter than the prefix
+     */
     public static boolean startsWithIgnoreCase(final String string, final String prefix) throws IllegalArgumentException, NullPointerException {
         if (string.length() < prefix.length()) {
             return false;
@@ -76,6 +110,13 @@ public class ProxyUtil {
         return string.regionMatches(true, 0, prefix, 0, prefix.length());
     }
 
+    /**
+     * Retrieves a map of all registered servers on the proxy.
+     * The map's keys are the server names, and the values are the corresponding
+     * RegisteredServer instances.
+     *
+     * @return a map of registered server names to their RegisteredServer objects.
+     */
     public static Map<String, RegisteredServer> getRegisteredServers() {
         Map<String, RegisteredServer> servers = new HashMap<>();
 
