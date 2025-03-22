23fec26dfde83ca6b18af28494c227d1eb5630f8
MineFact <minefact.minecraft@gmail.com>
2025-03-22 19:51:08 +0100

    fix missing slf4j dependency in controller module

--- controller/src/main/java/net/buildtheearth/terrabungee/controller/network/S2C/S2CServiceMessagePacket.java
+++ controller/src/main/java/net/buildtheearth/terrabungee/controller/network/S2C/S2CServiceMessagePacket.java
@@ -5,10 +5,25 @@ import net.buildtheearth.api.network.IS2CPacket;
 import net.buildtheearth.api.network.Response;
 import net.buildtheearth.api.network.ServicePacket;
 import net.buildtheearth.terrabungee.common.Constants;
+import net.buildtheearth.terrabungee.controller.TerraBungeeController;
 import net.buildtheearth.terrabungee.controller.network.C2S.C2SServiceMessagePacket;
 import net.buildtheearth.terrabungee.controller.network.NetworkManager;
 import net.buildtheearth.terrabungee.controller.services.ServiceManager;
 
+/** This is a service message packet that is sent from the proxy server to the controller.
+ * The controller will forward the message to the target service.
+ * <p>
+ * Service Message Types:<br>
+ * - DIRECT_MESSAGE<br>
+ * - DIRECT_MESSAGE_RECEIVED<br>
+ * - DIRECT_MESSAGE_FAILED<br>
+ * - BROADCAST<br>
+ * - GLOBAL_MESSAGE<br>
+ * - BAN<br>
+ * - WARNING<br>
+ * - STAFF<br>
+ * - PREMIUM<br>
+ * **/
 public class S2CServiceMessagePacket implements IS2CPacket {
     @Override
     public String getID() {
@@ -17,10 +32,12 @@ public class S2CServiceMessagePacket implements IS2CPacket {
 
     @Override
     public void onMessage(ServicePacket servicePacket, JsonObject data, Response response) {
+        TerraBungeeController.logger.info("Received service message packet from " + servicePacket.getId());
         String to = data.get("to").getAsString();
         if (ServiceManager.getInstance().getService(to) == null) {
             return;
         }
         NetworkManager.getInstance().send(new C2SServiceMessagePacket(servicePacket.getId(), to, data.get("message").getAsString()));
+        TerraBungeeController.logger.info("Sent service message packet to " + to);
     }
 }
