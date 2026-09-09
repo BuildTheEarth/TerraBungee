059f86fb1dec0a3c193d154e4e8fb73309957af7
Zoriot <zoriot@proton.me>
2026-09-09 18:10:26 +0200

    Remove old files

--- old/node/src/main/java/com/noahhusby/terrabungee/node/TerraBungeeNodeApplication.java
+++
@@ -1,7 +0,0 @@
-package com.noahhusby.terrabungee.node;
-
-public class TerraBungeeNodeApplication {
-    public static void main(String[] args) {
-        new TerraBungeeNode().onEnable();
-    }
-}
